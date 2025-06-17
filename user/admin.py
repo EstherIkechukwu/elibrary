328268b45b4b470730eb5c5643b7f66fa34f355c
Esther Ikechukwu <estherik18@gmail.com>
2025-06-17 11:40:42 +0100

    Implemented base user admin

--- user/admin.py
+++ user/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import Author
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Author, User
 
 # Register your models here.
 
-@admin.register(Author)
-class AuthorAdmin(UserAdmin):
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
     pass
+
+@admin.register(Author)
+class AuthorAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "usable_password", "password1", "password2", "first_name", "last_name", "email", "phone", "dob"),
+            },
+        ),
+    )
+    list_display = ['first_name', 'last_name', 'email', 'phone', 'dob', 'dod']
+    list_display_links = ['email', 'dob', 'dod']
+    list_editable = ['first_name', 'last_name', 'phone']
 
