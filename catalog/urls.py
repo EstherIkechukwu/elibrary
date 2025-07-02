a88d896c1392e9e09d377a59775b0d2ce0d5b62c
Esther Ikechukwu <estherik18@gmail.com>
2025-07-02 16:40:14 +0100

    Progress with django project, rest frameworks

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.get_books),
 
-    path("authors/", views.add_author, name="add_author"),
+    path("authors/", views.AddAuthorView.as_view(), name="add_author"),
 
-    path("authors/<int:pk>/", views.update_author, name="update_author"),
+    path("authors/<int:pk>/", views.GetUpdateDeleteAuthorView.as_view(), name="update_author"),
 
     path("get/authors/", views.get_authors, name="get_authors"),
 
--- catalog/views.py
+++ catalog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.generics import CreateAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 
 from catalog.models import Book
@@ -25,6 +26,14 @@ def add_author(request):
     author.save()
     return Response(author.data, status=status.HTTP_201_CREATED)
 
+class AddAuthorView(ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class GetUpdateDeleteAuthorView(RetrieveUpdateDestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
 @api_view(['GET'])
 def get_authors(request):
     authors = Author.objects.all()
@@ -38,3 +47,13 @@ def update_author(request, id):
     if serializer.is_valid(raise_exception=True):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['DELETE'])
+def delete_author(request, pk):
+    author = Author.objects.get(pk=pk)
+    author.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+def greet(request, name):
+    return render(request, 'index.html', context={'name': name})
