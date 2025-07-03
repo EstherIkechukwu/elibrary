42e229ed25c016f49acec4fdf6be3620647e19f1
Esther Ikechukwu <estherik18@gmail.com>
2025-07-03 13:11:39 +0100

    Added Book image view set

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,7 +7,7 @@ from .views import BookViewSet, BookImageViewSet
 router = routers.DefaultRouter()
 router.register('books', BookViewSet, basename='books')
 
-router.register('images', BookImageViewSet, basename='images')
+router.register('images', BookImageViewSet, basename='book-images')
 
 print(router.urls)
 
@@ -18,11 +18,15 @@ urlpatterns = [
 
     path("authors/", views.AddAuthorView.as_view(), name="add_author"),
 
-    path("authors/<int:pk>/", views.GetUpdateDeleteAuthorView.as_view(), name="update_author"),
+    path("authors/<int:pk>/", views.GetUpdateDeleteAuthorView.as_view()),
 
+    path("image/<int:pk>/", views.image_detail, name="book-images-detail"),
+
+    # path("update/authors/<int:pk>", views.update_author, name="update_author"),
+    # path("delete/authors/<int:pk>", views.delete_author, name="delete_author"),
     path("get/authors/", views.get_authors, name="get_authors"),
 
     path("greet/<name>", views.greet),
 
-    path("image/<int:pk>/", views.image_detail, name="book-images-detail"),
+
 ]
