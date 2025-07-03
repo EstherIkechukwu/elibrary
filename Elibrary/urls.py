c49b2a1f8433307eaec88f84d8de7b37ddccb5c7
Esther Ikechukwu <estherik18@gmail.com>
2025-07-03 16:58:01 +0100

    swagger

--- Elibrary/urls.py
+++ Elibrary/urls.py
@@ -22,6 +22,28 @@ from django.conf.urls.static import static
 admin.site.site_header = 'Library Management System'
 admin.site.site_title = 'library Admins'
 
+
+...
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+...
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny,],
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("catalog/", include("catalog.urls")),
@@ -29,7 +51,12 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 
+    path('swagger.<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
